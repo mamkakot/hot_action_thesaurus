7cd9b0af8fa48ce640d11a15355cca967c8f563b
Дмитрий Калинин <mitya.kalinin1@gmail.com>
2024-03-09 11:12:00 +0700

    started adding player and game models

---
+++ lib/application/game_options/game_options_provider.g.dart
@@ -0,0 +1,25 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'game_options_provider.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$gameOptionsHash() => r'eb0379cb247acc4d5829a952ba35b58c402eae5a';
+
+/// See also [GameOptions].
+@ProviderFor(GameOptions)
+final gameOptionsProvider =
+    AutoDisposeAsyncNotifierProvider<GameOptions, GameOptionsModel>.internal(
+  GameOptions.new,
+  name: r'gameOptionsProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product') ? null : _$gameOptionsHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$GameOptions = AutoDisposeAsyncNotifier<GameOptionsModel>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
